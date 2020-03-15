6f656b8f6818c4a66bed3192192fe6ee96ed91bc
rwitzlib <rob.witzlib@gmail.com>
2020-03-15 17:59:44 -0500

    initial

---
+++ Leave-Management/Data/Migrations/20200315215238_FixedLeaveHistoryAndAllocation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Leave_Management.Data.Migrations
+{
+    public partial class FixedLeaveHistoryAndAllocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataeCreated",
+                table: "LeaveAllocations");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "LeaveAllocations",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "LeaveAllocations");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataeCreated",
+                table: "LeaveAllocations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
---
+++ Leave-Management/Models/LeaveAllocationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Models
+{
+    public class LeaveAllocationViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public int NumberOfDays { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public EmployeeViewModel Employee { get; set; }
+        public string EmployeeId { get; set; }
+
+        public DetailsLeaveTypeViewModel LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public IEnumerable<SelectListItem> Emplyoees { get; set; }
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+    }
+}
