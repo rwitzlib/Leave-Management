beea52b112774277e6b569e7edecaad82e5b18d8
rwitzlib <rob.witzlib@gmail.com>
2020-05-12 00:44:25 -0500

    Implemented Theme and Added Required Files

--- Leave-Management/Models/LeaveRequestVM.cs
+++ Leave-Management/Models/LeaveRequestVM.cs
@@ -47,8 +47,11 @@ namespace Leave_Management.Models
     {
         [Display(Name = "Total Number of Requests")]
         public int TotalRequests { get; set; }
+        [Display(Name = "Approved Requests")]
         public int ApprovedRequests { get; set; }
+        [Display(Name = "Pending Requests")]
         public int PendingRequests { get; set; }
+        [Display(Name = "Rejected Requests")]
         public int RejectedRequests { get; set; }
         public List<LeaveRequestVM> LeaveRequests { get; set; }
     }
